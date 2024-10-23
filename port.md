994903d3397bb24e62df300954a06974ec827dde
Estecka <estecka@galacsys.com>
2024-10-23 18:38:12 +0200

    Fixed Api

--- port.md
+++ port.md
@@ -5,5 +5,6 @@ Initial Release
 ### 1.21.2
 #### No Workarounds:
 - `Gamerules.accept` is no longer static
+- `Gamerules::new` Now require feature flags
 #### Yarn Mappings
 - Some rules' `validate` were renamed to `validateAndSet`
--- src/main/java/tk/estecka/clothgamerules/api/ClothGamerulesScreenFactory.java
+++ src/main/java/tk/estecka/clothgamerules/api/ClothGamerulesScreenFactory.java
@@ -7,7 +7,6 @@ import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.text.Text;
 import net.minecraft.world.GameRules;
 import tk.estecka.clothgamerules.GamerulesMenuFactory;
-import static tk.estecka.clothgamerules.GamerulesMenuFactory.DEFAULT_TITLE;
 
 public interface ClothGamerulesScreenFactory
 {
@@ -56,18 +55,18 @@ public interface ClothGamerulesScreenFactory
 	}
 
 	static public Screen CreateScreen(Screen parent, GameRules rules, Consumer<Optional<GameRules>> onClose){
-		return GamerulesMenuFactory.CreateScreen(parent, DEFAULT_TITLE, rules, new GameRules(FeatureSet.empty()), onClose);
+		return new Builder().Parent(parent).ActiveValues(rules).OnClosed(onClose).Build();
 	}
 
 	static public Screen CreateScreen(Screen parent, GameRules rules, GameRules resetValues, Consumer<Optional<GameRules>> onClose){
-		return GamerulesMenuFactory.CreateScreen(parent, DEFAULT_TITLE, rules, resetValues, onClose);
+		return new Builder().Parent(parent).ActiveValues(rules).ResetValues(resetValues).OnClosed(onClose).Build();
 	}
 
 	static public Screen CreateScreen(Screen parent, Text title, GameRules rules, Consumer<Optional<GameRules>> onClose){
-		return GamerulesMenuFactory.CreateScreen(parent, title, rules, new GameRules(FeatureSet.empty()), onClose);
+		return new Builder().Parent(parent).Title(title).ActiveValues(rules).OnClosed(onClose).Build();
 	}
 
 	static public Screen CreateScreen(Screen parent, Text title, GameRules rules, GameRules resetValues, Consumer<Optional<GameRules>> onClose){
-		return GamerulesMenuFactory.CreateScreen(parent, title, rules, resetValues, onClose);
+		return new Builder().Parent(parent).Title(title).ActiveValues(rules).ResetValues(resetValues).OnClosed(onClose).Build();
 	}
 }
